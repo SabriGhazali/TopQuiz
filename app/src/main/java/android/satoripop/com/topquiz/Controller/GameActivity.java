addedc2ffa9c57844c0a824dbc659a4e0a2660b1
SabriGhazali <ghazalisabri125@gmail.com>
2019-06-17 17:54:40 +0200

    Display Question Add Score

--- app/src/main/java/android/satoripop/com/topquiz/Controller/GameActivity.java
+++ app/src/main/java/android/satoripop/com/topquiz/Controller/GameActivity.java
@@ -1,37 +1,71 @@
 package android.satoripop.com.topquiz.Controller;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.satoripop.com.topquiz.Model.Question;
 import android.satoripop.com.topquiz.Model.QuestionBank;
 import android.satoripop.com.topquiz.R;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
 
-
-    private TextView QuestionText;
-    private Button Answer1,Answer2,Answer3,Answer4;
+    private TextView mQuestionText;
+    private Button mAnswer1,mAnswer2,mAnswer3,mAnswer4;
     private QuestionBank mQuestionBank;
+    private Question mCurrentQuestion;
+    private int mNumberOfQuestions;
+    private int mScore=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        QuestionText= findViewById(R.id.activity_game_question_text);
-        Answer1= findViewById(R.id.activity_game_answer1_btn);
-        Answer2= findViewById(R.id.activity_game_answer2_btn);
-        Answer3= findViewById(R.id.activity_game_answer3_btn);
-        Answer4= findViewById(R.id.activity_game_answer4_btn);
-        //Remlpissage Des Questions
+        mQuestionText= findViewById(R.id.activity_game_question_text);
+        mAnswer1= findViewById(R.id.activity_game_answer1_btn);
+        mAnswer2= findViewById(R.id.activity_game_answer2_btn);
+        mAnswer3= findViewById(R.id.activity_game_answer3_btn);
+        mAnswer4= findViewById(R.id.activity_game_answer4_btn);
+//Remlpissage Des Questions
         mQuestionBank = this.generateQuestions();
+        mNumberOfQuestions = 4;
+        mCurrentQuestion=mQuestionBank.getQuestion();
+        displayQuestion(mCurrentQuestion);
+
+        // Use the tag property to 'name' the buttons
+        mAnswer1.setTag(0);
+        mAnswer2.setTag(1);
+        mAnswer3.setTag(2);
+        mAnswer4.setTag(3);
+        mAnswer1.setOnClickListener(this);
+        mAnswer2.setOnClickListener(this);
+        mAnswer3.setOnClickListener(this);
+        mAnswer4.setOnClickListener(this);
 
 
 
 
+
+
+
+
+
+
+    }
+
+    private void displayQuestion(final Question question) {
+        // Set the text for the question text view and the four buttons
+        mQuestionText.setText(question.getQuestion());
+        mAnswer1.setText(question.getChoiceList().get(0));
+        mAnswer2.setText(question.getChoiceList().get(1));
+        mAnswer3.setText(question.getChoiceList().get(2));
+        mAnswer4.setText(question.getChoiceList().get(3));
 
     }
 
@@ -57,10 +91,48 @@ public class GameActivity extends AppCompatActivity {
                         "666",
                         "742"),
                 3);
+        Question question4 = new Question("Who is the creator of this Application?",
+                Arrays.asList("El PAPPI",
+                        "Nogo Jari",
+                        "Bolbol Moulinex",
+                        "Sabri Las Vegas"),
+                3);
 
         return new QuestionBank(Arrays.asList(question1,
                 question2,
-                question3));
+                question3,question4));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Get Button Clicked !
+        int responseIndex = (int) v.getTag();
+
+        if (responseIndex==mCurrentQuestion.getAnswerIndex())
+        {Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        mScore++;
+        }
+
+        if (--mNumberOfQuestions == 0) {
+            // No question left, end the game
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+            builder.setTitle("Well done!")
+                    .setMessage("Your score is " + mScore)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .create()
+                    .show();
+        } else {
+            mCurrentQuestion = mQuestionBank.getQuestion();
+            displayQuestion(mCurrentQuestion);
+        }
 
     }
 }
--- app/src/main/java/android/satoripop/com/topquiz/Controller/MainActivity.java
+++ app/src/main/java/android/satoripop/com/topquiz/Controller/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +31,9 @@ public class MainActivity extends AppCompatActivity {
         mGreetingText =  findViewById(R.id.activity_main_greeting_txt);
         mNameInput =  findViewById(R.id.activity_main_name_input);
         mPlayButton = findViewById(R.id.activity_main_play_btn);
-
         mPlayButton.setEnabled(false);
+
+         mUser=new User();
 
         mNameInput.addTextChangedListener(new TextWatcher() {
             @Override
